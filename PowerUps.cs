01f3bf893decd2304a452a48899b8c54f8031c24
DBurts0 <62674279+DBurts0@users.noreply.github.com>
2020-08-06 11:06:38 -0500

    Add files via upload

---
+++ PowerUps.cs
@@ -0,0 +1,45 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps
+{
+    // Create a variable for health changing pickups
+    public float healthChanger;
+
+    // Create a variable for speed changing pickups
+    public float speedMultiplier;
+
+    // Create a variable for firerate changing pickups
+    public float firerateMultiplier;
+
+    // Create a variable for detection range changing pickups
+    public float detectionRangeChanger;
+
+    // Create a variable for how long the non-permanent powerups will last
+    public float duration;
+
+    // Create a variable determining if the powerup will be permanent
+    public bool isPermanent;
+    
+    // Create a variable to store information of the tanks
+    public GameObject tank;
+
+    // Create a variable to access the TankData Script
+    public TankData data;
+
+    void Start()
+    {
+        data = tank.GetComponent<TankData>();
+    }
+
+    public void OnActivate (TankData target)
+    {
+
+    }
+
+    public void OnDeactivate(TankData target)
+    {
+
+    }
+}
---
+++ PowerupController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupController : MonoBehaviour
+{
+
+    // Create a list
+    public List<PowerUps> powerupList;
+
+    // Create a variable to access the TankData script
+    public TankData data;
+
+    // Create a variable to access the PowerUps script
+    public PowerUps powerup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerupList = new List<PowerUps>();
+        data = GetComponent<TankData>();
+        powerup = GetComponent<PowerUps>();
+    }
+
+    // Create a function to add active powerups to the list
+    public void Add(PowerUps activepowerup)
+    {
+        // Activate the powerup
+        powerup.OnActivate(data);
+
+        // Check if the powerup is not permamnent
+        if (!activepowerup.isPermanent)
+        {
+            // Add the non permamnet powerup to the list
+            powerupList.Add(activepowerup);
+        }
+    }
+    
+    // Create a function to remove powerups from the list
+    public void Remove(PowerUps activePowerup)
+    {
+        powerup.OnDeactivate(data);
+        powerupList.Remove(activePowerup);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<PowerUps> expiredPowerups = new List<PowerUps>();
+        // Create a loop that goes through each power up in the list
+        foreach (PowerUps power in powerupList)
+        {
+            // Countdown
+            power.duration -= Time.deltaTime;
+
+            // If the timer runs out
+            if (power.duration <= 0)
+            {
+                // Remove the powerup from the list
+                expiredPowerups.Add(power);
+            }
+        }
+
+        // Remove powerups from the list of active powerups that are on the expired list
+        foreach (PowerUps power in expiredPowerups)
+        {
+            power.OnDeactivate(data);
+            powerupList.Remove(power);
+        }
+        expiredPowerups.Clear();
+    }
+}
